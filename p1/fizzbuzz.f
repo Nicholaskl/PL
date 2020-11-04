a414cc4452d144092dbc5e15b73f5961bfa7faa0
Nicholas <nicholasklvanahooper@gmail.com>
2020-11-04 12:09:25 +0800

    clean up p1

--- p1/fizzbuzz.f
+++ p1/fizzbuzz.f
@@ -6,21 +6,23 @@ c Modified: 7/10/2020
 c Purpose:  Basic fizzbuzz program in fortran
 c ---------------------------------------------------
 
-        program sumloop
+        program fizzbuzz
         
         integer i
 
 c Loops through from 1-100
-
         do 10 i = 1, 100
+c Fizzbuzz occurs every multiple of 15
             if(mod(i, 15) .EQ. 0) then
                 write (*,*) 'fizz buzz'
             else
+c Otherwise fizz every multiple of 3 and buzz every one of 5
                 if (mod(i, 3) .EQ. 0) then
                     write (*,*) 'fizz'
                 elseif (mod(i, 5) .EQ. 0) then
                     write (*,*) 'buzz'
                 else
+c If not a multiple of 3,5,15 then print number
                     write (*,*) i
                 endif
             endif
